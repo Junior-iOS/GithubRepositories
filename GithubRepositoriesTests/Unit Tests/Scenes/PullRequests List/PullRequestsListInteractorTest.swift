57a3d57a33673de43c1f43847df543a12f53d0a5
Nelson Junior <nelson.silva@grupopan.com>
2020-07-24 01:55:31 -0300

    PullRequestsListInteractor tested

--- GithubRepositoriesTests/Unit Tests/Scenes/PullRequests List/PullRequestsListInteractorTest.swift
+++ GithubRepositoriesTests/Unit Tests/Scenes/PullRequests List/PullRequestsListInteractorTest.swift
@@ -7,3 +7,57 @@
 //
 
 import Foundation
+import XCTest
+import Quick
+import Nimble
+import PromiseKit
+@testable import GithubRepositories
+
+class PullRequestsListInteractorTest: QuickSpec {
+    
+    override func spec() {
+        super.spec()
+        describe("PullRequestsListInteractorTest") {
+            var sut: PullRequestsListInteractor!
+            var mockPresenter: PullRequestsListPresenterMock!
+            var mockWorker: PullRequestsListWorkerMock!
+            
+            let repository = Repository(name: "name", description: "description", stars: 0, forks: 0, owner: Owner(name: "name", avatar: "avatar"))
+            
+            beforeEach {
+                PromiseKit.conf.Q.map = nil
+                PromiseKit.conf.Q.return = nil
+                
+                mockWorker = PullRequestsListWorkerMock()
+                mockPresenter = PullRequestsListPresenterMock()
+                
+                sut = PullRequestsListInteractor(worker: mockWorker)
+                sut.presenter = mockPresenter
+            }
+            
+            describe("fetching and population a list by making a request") {
+                
+                context("Request success") {
+                    it("Existing pull requests") {
+                        mockWorker.resultType = .success
+                        sut.repository = repository
+                        sut.fetchPullRequestsList()
+                        
+                        expect(mockPresenter.presentStopActivityIndicatorCount).to(equal(1))
+                        expect(mockPresenter.presentReloadTableCount).to(equal(1))
+                    }
+                    
+                    it("Emptyt pull requests") {
+                        mockWorker.resultType = .empty
+                        sut.repository = repository
+                        sut.fetchPullRequestsList()
+                        
+                        expect(mockPresenter.presentEmptyStateCount).to(equal(1))
+                    }
+                }
+                
+            }
+        }
+    }
+    
+}
